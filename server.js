4dd25f3548cf076b84bd8af3388b683a99db969f
chunga <clintonchunga@gmail.com>
2021-01-19 15:26:04 +0100

    creating my sequelize server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: 'true'}));
+
+
+db.sequelize.async()
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`server connected to ${PORT}`)
+    })
+})
